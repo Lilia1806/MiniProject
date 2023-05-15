fa3be4060b55d54120ef68ef7563fb3e3554ed4b
RenHeyzer <vesolyyradin@gmail.com>
2023-05-15 20:13:25 +0600

    добавил переход во второй фрагмент

--- app/src/main/java/com/example/miniproject/FirstFragment.kt
+++ app/src/main/java/com/example/miniproject/FirstFragment.kt
@@ -5,6 +5,7 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.navigation.fragment.findNavController
 import com.example.miniproject.databinding.FragmentFirstBinding
 
 class FirstFragment : Fragment() {
@@ -13,8 +14,19 @@ class FirstFragment : Fragment() {
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
+    ): View {
         binding = FragmentFirstBinding.inflate(inflater, container, false)
         return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        setupListener()
+    }
+
+    private fun setupListener() {
+        binding.btn1.setOnClickListener {
+            findNavController().navigate(R.id.action_firstFragment_to_secondFragment)
+        }
     }
 }
